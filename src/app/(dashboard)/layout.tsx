a9d5c602a0a29677a6c79836ac99ca74119e09d9
Manir <manir@Manirs-MacBook-Air.local>
2025-01-24 20:02:57 +0100

    feat: add School Management Dashboard UI design and component structure

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
+import Menu from "../components/Menu";
+import Navbar from "../components/Navbar";
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="h-screen flex">
+      {/* left sidebar */}
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%]">
+        <Link
+          href="/"
+          className="flex items-center justify-center lg:justify-start gap-2"
+        >
+          <Image src="/logo.png" width={32} height={32} alt="logo" />
+          <span className="hidden lg:block"> School</span>
+        </Link>
+        <Menu />
+      </div>
+      {/* right content */}
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+        <Navbar />
+        {children}
+      </div>
+    </div>
+  );
+}
